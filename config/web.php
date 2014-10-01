f6ae3e00ae07f9f2e62260baa3ea66c7303db72a
yafithekid212@gmail.com <yafithekid212@gmail.com>
2014-10-01 15:03:59 +0700

    fixing url

--- config/web.php
+++ config/web.php
@@ -38,6 +38,11 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ]
     ],
     'params' => $params,
 ];
