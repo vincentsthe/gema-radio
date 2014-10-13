2ad6b2d40f7d3cd790123aaed4fc1a50ea44822b
vincent The <vincent.s.the@gmail.com>
2014-10-13 21:22:37 +0700

    add preview functionality to transaksi

---
+++ modules/petugas/views/transaksi/preview.php
@@ -0,0 +1,231 @@
+<?php
+	
+	use yii\helpers\html;
+	use app\helpers\TimeHelper;
+
+	$this->title = 'Transaksi';
+?>
+
+<br>
+<h2 class="text-center">Bukti Pembayaran Kas</h2>
+
+<div class="row">
+	<div class="col-md-12">
+		<h4>No. <?= $transaksi->akun_id . "-" . TimeHelper::timeStampToFormattedDate(TimeHelper::formattedDateToTimestamp($transaksi->tanggal, '%Y-%m-%d'), 'd/m/Y') . "-" . $transaksi->id ?></h4>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Dibayarkan Kepada</h5>
+	</div>
+	<div class="col-md-8">
+		<h5><?= $transaksi->nama; ?></h5>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Uang Sebanyak</h5>
+	</div>
+	<div class="col-md-8">
+		<h5><?= $transaksi->nominal; ?></h5>
+	</div>
+</div>
+
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Terbilang</h5>
+	</div>
+	<div class="col-md-8">
+		<h5><?= $transaksi->terbilang; ?></h5>
+	</div>
+</div>
+
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Jenis Penyiaran</h5>
+	</div>
+	<div class="col-md-8">
+		<h5><?= $transaksi->jenis_transaksi; ?></h5>
+	</div>
+</div>
+
+<?php if($transaksi->jenis_transaksi == 'Iklan nasional'): ?>
+	<div class="row">
+		<div class="col-md-3 col-md-offset-1">
+			<h5>Order No</h5>
+		</div>
+		<div class="col-md-8">
+			<h5><?= $transaksi->no_order; ?></h5>
+		</div>
+	</div>
+<?php endif; ?>
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Siaran</h5>
+	</div>
+	<div class="col-md-8">
+		<div class="row">
+			<div class="col-md-2">
+				<h5><?= $transaksi->jumlah_siaran ?></h5>
+			</div>
+			<div class="col-md-10">
+				<h5>siaran</h5>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+				<h5><?= $transaksi->siaran_per_hari ?></h5>
+			</div>
+			<div class="col-md-10">
+				<h5>kali/hari</h5>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+				<h5><?= $transaksi->teks_spot ?></h5>
+			</div>
+			<div class="col-md-10">
+				<h5>teks spot</h5>
+			</div>
+		</div>
+	</div>
+</div>
+
+<br>
+<table class="table table-bordered">
+	<tr>
+		<th>Hari</th>
+		<th>Tanggal</th>
+		<th>Jam Siaran</th>
+	</tr>
+	<?php foreach($siarans as $siaran): ?>
+		<tr>
+			<td><?= TimeHelper::getDay($siaran['tanggal'], '%Y-%m-%d') ?></td>
+			<td><?= TimeHelper::getDate($siaran['tanggal'], '%Y-%m-%d') ?></td>
+			<td><?= $siaran['jamMulai'] ?></td>
+		</tr>
+	<?php endforeach;?>
+</table>
+
+<br><br>
+<hr>
+
+<h2 class="text-center">Bukti Pembayaran Kas</h2>
+
+<h2 class="text-right" style="color:#555;margin-right:40px;">Arsip</h2>
+
+<div class="row">
+	<div class="col-md-12">
+		<h4>No. <?= $transaksi->akun_id . "-" . TimeHelper::timeStampToFormattedDate(TimeHelper::formattedDateToTimestamp($transaksi->tanggal, '%Y-%m-%d'), 'd/m/Y') . "-" . "001"?></h4>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Dibayarkan Kepada</h5>
+	</div>
+	<div class="col-md-8">
+		<h5><?= $transaksi->nama; ?></h5>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Uang Sebanyak</h5>
+	</div>
+	<div class="col-md-8">
+		<h5><?= $transaksi->nominal; ?></h5>
+	</div>
+</div>
+
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Terbilang</h5>
+	</div>
+	<div class="col-md-8">
+		<h5><?= $transaksi->terbilang; ?></h5>
+	</div>
+</div>
+
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Jenis Penyiaran</h5>
+	</div>
+	<div class="col-md-8">
+		<h5><?= $transaksi->jenis_transaksi; ?></h5>
+	</div>
+</div>
+
+<?php if($transaksi->jenis_transaksi == 'Iklan nasional'): ?>
+	<div class="row">
+		<div class="col-md-3 col-md-offset-1">
+			<h5>Order No</h5>
+		</div>
+		<div class="col-md-8">
+			<h5><?= $transaksi->no_order; ?></h5>
+		</div>
+	</div>
+<?php endif; ?>
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-1">
+		<h5>Siaran</h5>
+	</div>
+	<div class="col-md-8">
+		<div class="row">
+			<div class="col-md-2">
+				<h5><?= $transaksi->jumlah_siaran ?></h5>
+			</div>
+			<div class="col-md-10">
+				<h5>siaran</h5>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+				<h5><?= $transaksi->siaran_per_hari ?></h5>
+			</div>
+			<div class="col-md-10">
+				<h5>kali/hari</h5>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+				<h5><?= $transaksi->teks_spot ?></h5>
+			</div>
+			<div class="col-md-10">
+				<h5>teks spot</h5>
+			</div>
+		</div>
+	</div>
+</div>
+
+<br>
+<table class="table table-bordered">
+	<tr>
+		<th>Hari</th>
+		<th>Tanggal</th>
+		<th>Jam Siaran</th>
+	</tr>
+	<?php foreach($siarans as $siaran): ?>
+		<tr>
+			<td><?= TimeHelper::getDay($siaran['tanggal'], '%Y-%m-%d') ?></td>
+			<td><?= TimeHelper::getDate($siaran['tanggal'], '%Y-%m-%d') ?></td>
+			<td><?= $siaran['jamMulai'] ?></td>
+		</tr>
+	<?php endforeach;?>
+</table>
+
+<br><br>
+<div class="text-center">
+	<a href="<?= \Yii::$app->urlManager->createUrl(['petugas/transaksi/createtransaksi']); ?>" class="btn btn-success">Kembali</a>
+	<a href="<?= \Yii::$app->urlManager->createUrl(['petugas/transaksi/save']); ?>" class="btn btn-success">Simpan</a>
+</div>
+
+<br><br><br>
