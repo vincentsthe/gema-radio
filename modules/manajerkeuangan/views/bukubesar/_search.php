4222063a6c3061985e1c2596e3de04cf7e1cf55f
yafithekid212@gmail.com <yafithekid212@gmail.com>
2014-11-01 20:11:44 +0700

    redesign buku besar interface

--- modules/manajerkeuangan/views/bukubesar/_search.php
+++ modules/manajerkeuangan/views/bukubesar/_search.php
@@ -8,15 +8,30 @@
 
 
 ?>
-
+<div class='col-xs-12'>
 <?php $form = ActiveForm::begin(['action'=>['index'],'method'=>'get']); ?>
+<?= $form->errorSummary($model); ?>
 
-<?= $form->field($model,'tanggal_awal')->textInput(['id'=>'tanggal_awal']); ?>
-<?= $form->field($model,'tanggal_akhir')->textInput(['id' =>'tanggal_akhir']); ?>
-<?= $form->field($model,'akun_id')->dropDownList(ArrayHelper::map($akuns,'id','nama')); ?>
-<?= Html::submitButton('Search',['class' => 'btn btn-success']); ?>
+    <div class='col-xs-3'>
+        Akun <?= Html::activeDropDownList($model,'akun_id',ArrayHelper::map($akuns,'id','nama'),['class'=>'form-control']); ?>
+        
+    </div>
+    <div class='col-xs-3'>
+        Mulai
+        <?= Html::activeTextInput($model,'tanggal_awal',['id' => 'tanggal_awal','class'=>'form-control']); ?>
+    </div>
+    <div class='col-xs-3'>
+        Akhir
+        <?= Html::activeTextInput($model,'tanggal_akhir',['id' => 'tanggal_akhir','class'=>'form-control']); ?>
+    </div>
+    <div class='col-xs-3'>
+        <br>
+        <?= Html::submitButton('Proses',['class' => 'btn btn-success']); ?>
+    </div>
+
 <?php ActiveForm::end(); ?>
-
+</div>
+<br><br><br>
 <?php
 $this->registerJs(
     '   $("#tanggal_awal").datetimepicker({
--- modules/manajerkeuangan/views/bukubesar/index.php
+++ modules/manajerkeuangan/views/bukubesar/index.php
@@ -1,6 +1,8 @@
 <?php
     use yii\grid\GridView;
     use yii\helpers\Html;
+    $debet = 0;
+    $kredit = 0;
 ?>
 <h2>Buku Besar</h2>
 <br>
@@ -9,7 +11,7 @@
     'dataProvider' => $dataProvider,
     'columns' => [
         ['class' => 'yii\grid\SerialColumn'],
-        'id',
+        //'id',
         'tanggal',
         'jenis_transaksi',
         [
@@ -29,3 +31,8 @@
     ],
 ]);
 ?>
+<div class='col-xs-6 col-xs-offset-6'>
+<table class='table table-condensed pull-right'>
+    <tr><td>Saldo</td><td><?=$debet;?></td><td><?=$kredit;?></td></tr>
+</table>
+</div>
